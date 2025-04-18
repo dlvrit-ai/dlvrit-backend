d031dbf32054375f33f8ce2c94841c0f9e324069
Zeb Chadfield <admin@dlvrit.ai>
2025-04-18 21:46:46 +0100

    switch stripe payment mode

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/create-checkout-session", async (req, res) => {
         quantity,
         promo: promo || "none"
       },
-      success_url: `${process.env.FRONTEND_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.FRONTEND_URL}/success.html`,
       cancel_url: `${process.env.FRONTEND_URL}/cancelled.html`
     });
 
